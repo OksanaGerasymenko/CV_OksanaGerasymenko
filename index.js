5336ff2443d94a82624252c04e180ff71da23b9e
Oksana Gerasimenko <gerrus155@gmail.com>
2022-07-10 00:31:57 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("menu-btn").onclick = () =>{
+    document.getElementById("menu").classList.toggle("active")
+    document.getElementById("menu-img").classList.toggle("active")   
+    document.getElementById("menu-img-active").classList.toggle("active")    
+}
+
